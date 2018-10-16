c7d0cc5b134374ebfc1af5f7c40c706e12929f77
Jasper Lee <bobmugura@gmail.com>
2018-10-15 20:12:31 -0400

    added shared module to reuse header component across color-tool and car-tool

--- src/app/car-tool/car-tool.module.ts
+++ src/app/car-tool/car-tool.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CarToolComponent } from './components/car-tool/car-tool.component';
 import { CarFormComponent } from './components/car-form/car-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    SharedModule,
   ],
   declarations: [CarToolComponent, CarFormComponent],
   exports: [CarToolComponent],
--- src/app/color-tool/color-tool.module.ts
+++ src/app/color-tool/color-tool.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ColorToolComponent } from './components/color-tool/color-tool.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ColorFormComponent } from './components/color-form/color-form.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    SharedModule,
   ],
   declarations: [ColorToolComponent, ColorFormComponent],
   exports: [ColorToolComponent],
