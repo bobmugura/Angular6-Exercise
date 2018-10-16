4fece8a99ed2237488b4783c20b079e8b9c7e3c2
Jasper Lee <bobmugura@gmail.com>
2018-10-15 20:50:20 -0400

    componetized car-table-row from car-table

--- src/app/car-tool/car-tool.module.ts
+++ src/app/car-tool/car-tool.module.ts
@@ -4,6 +4,8 @@ import { CarToolComponent } from './components/car-tool/car-tool.component';
 import { CarFormComponent } from './components/car-form/car-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { CarTableComponent } from './components/car-table/car-table.component';
+import { CarTableRowComponent } from './components/car-table-row/car-table-row.component';
 
 
 @NgModule({
@@ -12,7 +14,7 @@ import { SharedModule } from '../shared/shared.module';
     ReactiveFormsModule,
     SharedModule,
   ],
-  declarations: [CarToolComponent, CarFormComponent],
+  declarations: [CarToolComponent, CarFormComponent, CarTableComponent, CarTableRowComponent],
   exports: [CarToolComponent],
 })
 export class CarToolModule { }
---
+++ src/app/car-tool/components/car-table-row/car-table-row.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Car } from '../../models/car';
+
+@Component({
+  selector: 'tr.car-table-row',
+  templateUrl: './car-table-row.component.html',
+  styleUrls: ['./car-table-row.component.css']
+})
+export class CarTableRowComponent implements OnInit {
+
+  @Input()
+  public car: Car;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
